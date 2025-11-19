d7a2db8cca669de9cf631a0d47509a9db167f0fe
Ali Rıza Yücefaydalı <alirizayucefaydali@Alis-MacBook-Pro.local>
2025-11-19 20:26:39 +0100

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import '../css/app.css';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { createApp, h } from 'vue';
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.vue`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
---
+++ resources/js/pages/HomePage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {computed, ref} from 'vue'
+import { Head } from '@inertiajs/vue3'
+
+
+const breadcrumbs = ref([
+    {
+        title: 'Home',
+        href: '/dashboard',
+    },
+])
+
+const pageTitle = computed(() => {
+    return breadcrumbs.value.length
+        ? breadcrumbs.value[breadcrumbs.value.length - 1].title
+        : 'Laravel Auth'   // Default title
+})
+
+</script>
+
+<template>
+    <Head :title="pageTitle"/>
+    <h1>Home Page</h1>
+</template>
+
+<style scoped>
+
+</style>
